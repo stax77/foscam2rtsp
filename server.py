082a85603f08bc9e4e34ae6c543525fe99a29fab
stax <abr.kadavra@gmail.com>
2023-01-29 14:33:52 +0300

    initial commit

---
+++ server.py
@@ -0,0 +1,339 @@
+import http.server
+import requests
+import uuid
+import argparse
+import re
+#from urllib3.util import SKIP_HEADER
+import urllib3
+from updatedrequesthandler import UpdatedRequestHandler
+import threading
+import socket
+import io
+
+# class SessionEntry :
+#     def __init__( self ) :
+#         self.session_id = 0
+#         self.session_client = ()
+#         self.
+
+class S(UpdatedRequestHandler):
+
+    def __init__( self, request, client_address, server ) :
+        self.sessionList = {}
+        super().__init__( request, client_address, server )
+        #asdlkasd
+        #qwiuoiqw
+
+    #sessionList = {}
+
+    # :_
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    # remove later
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
+    def do_SETUP(self):
+        # если в свойствах есть сессия, то ругнуться по RFC
+        # если нет, то создать новую и записать параметры клиента + target path?
+        # если стрим, уже вещается, то сделать что? )
+        # отправить ответ клиенту
+        #print( self.protocol_version )
+        #self._set_headers()
+        #self.wfile.write(self._html("hi!"))
+        #print( self.path )
+        #print( self.requestline )
+        #print( "setup" )
+        #print( self.client_address )
+        #print( self.headers )
+        #
+        cseq = self.headers["CSeq"]
+        if "Session" in self.headers :
+            self.send_response( 459, "Aggregate Operation Not Allowed" )
+            self.send_header( "CSeq", cseq )
+            self.end_headers()
+            return
+        
+        #
+        session = uuid.uuid4().hex
+        transport = self.headers["Transport"]
+        
+        #
+        reg = re.search( "client_port=(\d+)-(?:\d+)", transport )
+        if reg is None :
+            self.send_response( 400, "Bad Client Port" )
+            self.send_header( "CSeq", cseq )
+            self.end_headers()
+            return
+
+        udp_port = int( reg.group( 1 ) )
+        udp_address = self.client_address[0]
+
+        #if self.sessionList is None :
+        #    self.sessionList = {}
+        sessionInfo = { "session" : session, "client" : self.client_address, "transport" : transport, "udp_address" : udp_address, "udp_port" : udp_port }
+        self.sessionList.update( { session : sessionInfo } )
+
+        #
+        print( f"setup: new session {session} for client {self.client_address}" )
+
+        #
+        self.send_response( 200 )
+        self.send_header( "CSeq", cseq )
+        self.send_header( "Session", session )
+        self.send_header( "Transport", transport ) # + ";server_port=15678-15679")
+        self.end_headers()
+        #
+
+
+# GET http://[IP]:[port]/livestream/[number]?action=play&media=[type] 
+# HTTP/1.1\r\n 
+# User-Agent: HiIpcam/V100R003 VodClient/1.0.0\r\n 
+# Connection: Keep-Alive\r\n 
+# Cache-Control: no-cache\r\n 
+# Authorization: [username] [password] \r\n 
+# Content-Length: [length] \r\n 
+# \r\n 
+# Cseq: 1\r\n 
+# Transport: RTP/AVP/TCP;unicast;interleaved=0-1\r\n 
+# \r\n 
+
+    def readBytes( self, stream : io.BufferedIOBase, count ) :
+        # create array
+        result = bytes()
+        #
+        while len( result ) < count :
+            data = stream.read( count - len( result ) )
+            if data is None : 
+                return None
+            result = result + data
+        #
+        return result
+
+
+    def playThread( self, udp_address, udp_port ) :
+
+        #
+        targetSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+        #
+        headers = {
+            'User-Agent' : 'HiIpcam/V100R003 VodClient/1.0.0',
+            'Connection' : 'Keep-Alive',
+            'Cache-Control' : 'no-cache',
+            'Authorization' : 'ipcam w54723',
+            #'Accept' : None,
+            #'Accept-Encoding' : urllib3.util.SKIP_HEADER,
+            #'Host' : urllib3.util.SKIP_HEADER
+        }
+
+        # data = {
+        #     'CSeq' : 1,
+        #     'Transport' : 'RTP/AVP/TCP;unicast;interleaved=0-1'
+        # }
+
+        content = "Cseq: 1\r\nTransport: RTP/AVP/TCP;unicast;interleaved=0-1\r\n"
+        raw_content = content.encode( "utf-8" )
+
+        with requests.get( "http://192.168.1.77:80/livestream/11?action=play&media=video_audio_data", headers=headers, data=raw_content, stream=True ) as response :
+            #print( response )
+            #print( response.headers )
+
+            #response.iter_lines( )
+            #hds = http.client.parse_headers( response.raw )
+            #print( hds.keys() )
+
+            #print( response.raw )
+
+            #
+            #for chunk in response.iter_content( chunk_size=1200 ) :
+            #    targetSocket.sendto( chunk, ( udp_address, udp_port ) )
+
+            # skip bytes till first $
+            # add check for count )
+            tb = b''
+
+            while True :
+                tb = self.readBytes( response.raw, 1 )
+                if tb == b'$' : 
+                    break
+
+            if tb != b'$' :
+                print( "initial $ not found" )
+                return              
+
+            #
+            while tb == b'$' :
+
+                # 
+#                print( "$ found" )
+
+                # skip 3 bytes
+                self.readBytes( response.raw, 3 )
+
+                # read length (int32, net-endianess)
+                raw_len = self.readBytes( response.raw, 4 )
+                len = int.from_bytes( raw_len, byteorder="big", signed=False )
+
+                # read RTP packet
+                raw_rtp = self.readBytes( response.raw, len )
+                targetSocket.sendto( raw_rtp, ( udp_address, udp_port ) )
+
+                #
+                tb = self.readBytes( response.raw, 1 )
+            # checkbyte = response.iter_content( 1, False )
+            # state = 0            
+            # for byte in checkbyte :
+            #     if state == 0 :
+            #         if  byte == '$' :
+            #             print( "symbol is ok" )
+            #         state = 1
+            #     elif state == 1 :
+            #         #print( "not ok" )
+            #         pass
+
+            #response.raw
+
+            print( "non matching $ found" )
+
+            #response.iter_content( )
+
+
+#        response.close()
+
+        pass
+
+    def do_PLAY( self ) :
+        # проверить пераметры session
+        # если нет, то ругнуться по RFC
+        # добавить эту сессию к списку слушателей для потока
+        # запустить поток, если не запущен
+        # сказать клиенту ок 
+        #print( "play" )
+        #print( self.client_address )
+        #print( self.headers )
+
+        cseq = self.headers["CSeq"]
+        session = self.headers["Session"]
+        #transport = self.headers["Transport"]
+
+        if session in self.sessionList :
+            self.send_response( 200 )
+            #
+            sessionInfo = self.sessionList[session]
+            thread = threading.Thread( target=self.playThread, args=( sessionInfo["udp_address"], sessionInfo["udp_port"] ), daemon=True )
+            thread.start()
+            # store threar for cleaning up?
+            sessionInfo["thread"] = thread
+        else :
+            self.send_response( 404 )
+
+        self.send_header( "CSeq", cseq )
+        self.end_headers()
+
+    #
+    def do_TEARDOWN( self ) :
+        # проверить session
+        # елси нет, то ругнуться
+        # если есть, то убрать сессию из списка слушателей
+        # если это последний слушатель, то остановить поток
+        # ответить клиенту
+        #print( "teardown" )
+        #print( self.client_address )
+        #print( self.headers )
+        cseq = self.headers["CSeq"]
+        session = self.headers["Session"]
+        #
+        if session in self.sessionList :
+            self.sessionList.pop( session )
+        #
+        print( f"teardown: session {session}" )
+        #transport = self.headers["Transport"]
+        self.send_response( 200 )
+        self.send_header( "CSeq", cseq )
+        #self.send_header( "Session", session )
+        #self.send_header( "Transport", transport + ";server_port=15678-15679")
+        self.end_headers()
+        pass
+
+    # def do_GET(self):
+    #     self._set_headers()
+    #     self.wfile.write(self._html("hi!"))
+
+    def do_OPTIONS( self ) :
+        return
+        # sliently ignore
+        print( "options" )
+        print( self.client_address )
+        print( self.headers )
+        
+    # def do_HEAD(self):
+    #     self._set_headers()
+
+    # def do_POST(self):
+    #     # Doesn't do anything with posted data
+    #     self._set_headers()
+    #     self.wfile.write(self._html("POST!"))
+
+    def notimplemented( self ) :
+        print( self.command, "Not implemented" )
+        self.send_response_only( 501, "Not implemented" )
+        self.end_headers()
+
+    def do_DESCRIBE( self ) :
+        self.notimplemented()
+
+    def do_ANNOUNCE( self ) :
+        self.notimplemented()
+
+    def do_GET_PARAMETER( self ) :
+        self.notimplemented()
+
+    def do_PAUSE( self ) :
+        self.notimplemented()
+    
+    def do_SET_PARAMETER( self ) :
+        self.notimplemented()
+
+    def do_REDIRECT( self ) :
+        self.notimplemented()
+
+    def do_RECORD( self ) :
+        self.notimplemented()
+
+
+def run(server_class=http.server.HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd = server_class(server_address, handler_class)
+    try :
+      httpd.serve_forever()
+    except KeyboardInterrupt :
+      httpd.server_close() 
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
---
+++ updatedrequesthandler.py
@@ -0,0 +1,86 @@
+import http.server
+from http import HTTPStatus
+
+class UpdatedRequestHandler(http.server.BaseHTTPRequestHandler) :
+
+    protocol_version = "RTSP/1.0"
+
+    def parse_request(self):
+        """Parse a request (internal).
+
+        The request should be stored in self.raw_requestline; the results
+        are in self.command, self.path, self.request_version and
+        self.headers.
+
+        Return True for success, False for failure; on failure, any relevant
+        error response has already been sent back.
+
+        """
+        self.command = None  # set in case of error on the first line
+        self.request_version = version = self.default_request_version
+        self.close_connection = True
+        requestline = str(self.raw_requestline, 'iso-8859-1')
+        requestline = requestline.rstrip('\r\n')
+        self.requestline = requestline
+        words = requestline.split()
+        if len(words) == 0:
+            return False
+
+        if len(words) >= 3:  # Enough to determine protocol version
+            version = words[-1]
+            try:
+                if not version.startswith('RTSP/'):
+                    raise ValueError
+                base_version_number = version.split('/', 1)[1]
+                version_number = base_version_number.split(".")
+                # RFC 2145 section 3.1 says there can be only one "." and
+                #   - major and minor numbers MUST be treated as
+                #      separate integers;
+                #   - HTTP/2.4 is a lower version than HTTP/2.13, which in
+                #      turn is lower than HTTP/12.3;
+                #   - Leading zeros MUST be ignored by recipients.
+                if len(version_number) != 2:
+                    raise ValueError
+                version_number = int(version_number[0]), int(version_number[1])
+            except (ValueError, IndexError):
+                self.send_error(
+                    HTTPStatus.BAD_REQUEST,
+                    "Bad request version (%r)" % version)
+                return False
+
+            if version_number != (1, 0):
+                self.send_error(
+                    HTTPStatus.HTTP_VERSION_NOT_SUPPORTED,
+                    "Invalid RTSP version (%s)" % base_version_number)
+                return False
+            self.request_version = version
+            self.close_connection = False
+
+        if len(words) != 3 :
+            self.send_error(
+                HTTPStatus.BAD_REQUEST,
+                "Bad request syntax (%r)" % requestline)
+            return False
+
+        command, path = words[:2]
+        self.command, self.path = command, path
+
+        # Examine the headers and look for a Connection directive.
+        try:
+            self.headers = http.client.parse_headers(self.rfile,
+                                                     _class=self.MessageClass)
+        except http.client.LineTooLong as err:
+            self.send_error(
+                HTTPStatus.REQUEST_HEADER_FIELDS_TOO_LARGE,
+                "Line too long",
+                str(err))
+            return False
+        except http.client.HTTPException as err:
+            self.send_error(
+                HTTPStatus.REQUEST_HEADER_FIELDS_TOO_LARGE,
+                "Too many headers",
+                str(err)
+            )
+            return False
+
+        return True
