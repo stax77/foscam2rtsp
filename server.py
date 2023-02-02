a1b1c736fa92c170bf3065eb300105a8f8cc9a8d
stax <abr.kadavra@gmail.com>
2023-02-02 15:17:21 +0300

    rewrite handling

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ import threading
 import socket
 import io
 import rtp_work
+import time
 
 # class SessionEntry :
 #     def __init__( self ) :
@@ -46,6 +47,7 @@ class S(UpdatedRequestHandler):
         # если нет, то создать новую и записать параметры клиента + target path?
         # если стрим, уже вещается, то сделать что? )
         # отправить ответ клиенту
+        #print( f"setup, {self.headers}" )
         #print( self.protocol_version )
         #self._set_headers()
         #self.wfile.write(self._html("hi!"))
@@ -55,52 +57,68 @@ class S(UpdatedRequestHandler):
         #print( self.client_address )
         #print( self.headers )
         #
-        cseq = self.headers["CSeq"]
         if "Session" in self.headers :
-            self.send_response( 459, "Aggregate Operation Not Allowed" )
-            self.send_header( "CSeq", cseq )
-            self.end_headers()
-            return
-        
-        #
-        session = uuid.uuid4().hex
+
+            session = self.headers["Session"]
+            sessionInfo = self.sessionList[session]
+
+        else :
+
+            # create sesstion ID and source socket
+            session = uuid.uuid4().hex
+
+            source_socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+            source_socket.bind( ( '', 0 ) )
+
+            sessionInfo = { "session" : session, "client" : self.client_address, "source_socket" : source_socket }
+            self.sessionList.update( { session : sessionInfo } )
+
+        # update transport info
         transport = self.headers["Transport"]
         
-        #
         reg = re.search( "client_port=(\d+)-(?:\d+)", transport )
         if reg is None :
             self.send_response( 400, "Bad Client Port" )
-            self.send_header( "CSeq", cseq )
+            self.send_header( "CSeq", self.headers["CSeq"] )
             self.end_headers()
             return
 
         udp_port = int( reg.group( 1 ) )
         udp_address = self.client_address[0]
 
-        #if self.sessionList is None :
-        #    self.sessionList = {}
-        sessionInfo = { "session" : session, "client" : self.client_address, "transport" : transport, "udp_address" : udp_address, "udp_port" : udp_port }
-        self.sessionList.update( { session : sessionInfo } )
+        sessionInfo["transport"] = transport
+        sessionInfo["udp_address"] = udp_address
+        sessionInfo["udp_port"] = udp_port
+
+        # get source socket port
+        ( _, source_port ) = sessionInfo["source_socket"].getsockname() 
 
         #
-        print( f"setup: new session {session} for client {self.client_address}" )
+        #print( f"setup: session {session} for client {self.client_address}" )
 
         #
         self.send_response( 200 )
-        self.send_header( "CSeq", cseq )
+        self.send_header( "CSeq", self.headers["CSeq"] )
+        self.send_header( "Public", "DESCRIBE, SETUP, TEARDOWN, PLAY, OPTIONS" )
         self.send_header( "Session", session )
-        self.send_header( "Transport", transport ) # + ";server_port=15678-15679")
+        self.send_header( "Transport", transport + f";server_port={source_port}-{source_port+1}")
         self.end_headers()
         #
 
     def do_DESCRIBE( self ) :
         #self.notimplemented()
+        #print( f"describe, {self.headers}" ) 
+        #content = "m=video 96 H264/90000/704/576\r\nm=audio 0 PCMU/8000/1\r\n"
+        sdp = [
+            "m=video 0 RTP/AVP 96",
+            "m=audio 0 RTP/AVP 0",
+            "a=rtpmap:96 H264/90000/704/576"
+        ]
+        content = "\r\n".join( sdp ) + "\r\n"
 
-        content = "m=video 96 H264/90000/704/576\r\nm=audio 0 PCMU/8000/1\r\n"
-
-        cseq = self.headers["CSeq"]
         self.send_response( 200 )
-        self.send_header( "CSeq", cseq )
+        self.send_header( "CSeq", self.headers["CSeq"] )
+        self.send_header( "Public", "DESCRIBE, SETUP, TEARDOWN, PLAY, OPTIONS" )
         self.send_header( "Content-Base", self.path )
         self.send_header( "Content-Type", "application/sdp" )
         self.send_header( "Content-Length", len( content ) )
@@ -108,20 +126,6 @@ class S(UpdatedRequestHandler):
 
         self.wfile.write( content.encode( "ascii" ) )
         
-
-
-
-# GET http://[IP]:[port]/livestream/[number]?action=play&media=[type] 
-# HTTP/1.1\r\n 
-# User-Agent: HiIpcam/V100R003 VodClient/1.0.0\r\n 
-# Connection: Keep-Alive\r\n 
-# Cache-Control: no-cache\r\n 
-# Authorization: [username] [password] \r\n 
-# Content-Length: [length] \r\n 
-# \r\n 
-# Cseq: 1\r\n 
-# Transport: RTP/AVP/TCP;unicast;interleaved=0-1\r\n 
-# \r\n 
 
     def readBytes( self, stream : io.BufferedIOBase, count ) :
         # create array
@@ -136,10 +140,60 @@ class S(UpdatedRequestHandler):
         return result
 
 
-    def playThread( self, udp_address, udp_port, event ) :
-
+    def sendRtp( self, raw_rtp, sessionInfo ) :
+        # do not cache UDP as transport info can change ) overwise pack sending to separate class?
+        udp_address = sessionInfo["udp_address"]
+        udp_port = sessionInfo["udp_port"]
         #
-        targetSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+        sessionInfo["source_socket"].sendto( raw_rtp, ( udp_address, udp_port ) )
+
+
+    def processRtp( self, raw_rtp, sessionInfo ) :
+        #
+        rtp_headers  = rtp_work.DecodeRTPpacket( bytes.hex( raw_rtp ) ) # payload is HEXed bytes
+        if rtp_headers["payload_type"] == 0 : # audio
+
+            self.sendRtp( raw_rtp, sessionInfo )
+            pass
+
+        elif rtp_headers["payload_type"] == 96 : # video
+
+            # chunk_size = 45000
+            # # handle split ? 
+            # # strip first two bytes (0x00000001 NAL prefix in byte0stream)
+            # payload = bytes.fromhex( rtp_headers['payload'] )[4:]
+            # chunk_start = 0
+            # while chunk_start < len( payload ) :
+            #     #
+            #     payload_block = payload[chunk_start:chunk_start+chunk_size-1]
+            #     chunk_start = chunk_start + chunk_size
+            #     #
+            #     rtp_headers['sequence_number'] = cseq_96
+            #     cseq_96 = cseq_96 + 1
+            #     rtp_data = bytes.fromhex( rtp_work.GenerateRTPpacket2( rtp_headers, bytes.hex( payload_block ) ) )
+            #     targetSocket.sendto( rtp_data, ( udp_address, udp_port ) )
+            #     time.sleep( 0.001 )
+                #
+            
+            if len( raw_rtp ) < 65000 :
+                rtp_payload = bytes.fromhex( rtp_headers['payload'] )[4:] # strip 4 bytes of NAL prefix
+                rtp_data = bytes.fromhex( rtp_work.GenerateRTPpacket2( rtp_headers, bytes.hex( rtp_payload ) ) )
+                self.sendRtp( rtp_data, sessionInfo )
+                pass                            
+
+# GET http://[IP]:[port]/livestream/[number]?action=play&media=[type] 
+# HTTP/1.1\r\n 
+# User-Agent: HiIpcam/V100R003 VodClient/1.0.0\r\n 
+# Connection: Keep-Alive\r\n 
+# Cache-Control: no-cache\r\n 
+# Authorization: [username] [password] \r\n 
+# Content-Length: [length] \r\n 
+# \r\n 
+# Cseq: 1\r\n 
+# Transport: RTP/AVP/TCP;unicast;interleaved=0-1\r\n 
+# \r\n 
+
+    def playThread( self, sessionInfo ) :
 
         #
         headers = {
@@ -152,32 +206,22 @@ class S(UpdatedRequestHandler):
             #'Host' : urllib3.util.SKIP_HEADER
         }
 
-        # data = {
-        #     'CSeq' : 1,
-        #     'Transport' : 'RTP/AVP/TCP;unicast;interleaved=0-1'
-        # }
+        content = {
+            'Cseq' : 1,
+            'Transport' : 'RTP/AVP/TCP;unicast;interleaved=0-1'
+        }
+        raw_content = ("\r\n".join( key + ": " + str( content[key] ) for key in content ) + "\r\n").encode( "utf-8" )
 
-        content = "Cseq: 1\r\nTransport: RTP/AVP/TCP;unicast;interleaved=0-1\r\n"
-        raw_content = content.encode( "utf-8" )
+        with requests.get( "http://192.168.1.77:80/livestream/11?action=play&media=video_audio_data", headers=headers, data=raw_content, stream=True ) as response :
 
-        with requests.get( "http://192.168.1.77:80/livestream/11?action=play&media=video", headers=headers, data=raw_content, stream=True ) as response :
-            #print( response )
-            #print( response.headers )
-
-            #response.iter_lines( )
-            #hds = http.client.parse_headers( response.raw )
-            #print( hds.keys() )
-
-            #print( response.raw )
+            if response.status_code != 200 :
+                print( f"domofon request {response.status_code}, {response.headers}" )
+                return
 
             #
-            #for chunk in response.iter_content( chunk_size=1200 ) :
-            #    targetSocket.sendto( chunk, ( udp_address, udp_port ) )
-
-            # skip bytes till first $
-            # add check for count )
             tb = b''
 
+            # skip headers
             while True :
                 tb = self.readBytes( response.raw, 1 )
                 if tb == b'$' : 
@@ -188,94 +232,32 @@ class S(UpdatedRequestHandler):
                 return              
 
             #
-            packet_num = 0
-            cseq_96 = 0
+            event = sessionInfo["thread_event"]
+            #
+            while tb == b'$' :
 
-            while tb == b'$' and not event.is_set() :
-
-                # 
-#                print( "$ found" )
+                if event.is_set() :
+                    return
 
                 # skip 3 bytes
                 raw_channel_id = self.readBytes( response.raw, 1 )
                 raw_reserve = self.readBytes( response.raw, 2 )
-                
-                #
 
                 # read length (int32, net-endianess)
                 raw_length = self.readBytes( response.raw, 4 )
-                # if len( raw_length ) != 4 :
-                #     print( "failed to read 4 bytes!")
-                #     return
-
                 length = int.from_bytes( raw_length, byteorder="big", signed=False )
-
-                #
-                #print( f"packet {packet_num}, channe_id {raw_channel_id}, length {length}" )
-
 
                 # read RTP packet
                 raw_rtp = self.readBytes( response.raw, length )
-                # if len( raw_rtp ) != length :
-                #     print( f"failed to read {length} bytes!")
-                #     return
+                self.processRtp( raw_rtp, sessionInfo )
 
-                #
-                rtp_headers  = rtp_work.DecodeRTPpacket( bytes.hex( raw_rtp ) ) # payload is HEXed bytes
-                if rtp_headers["payload_type"] == 0 : # audio
-
-                    targetSocket.sendto( raw_rtp, ( udp_address, udp_port ) )
-
-                elif rtp_headers["payload_type"] == 96 : # video
-                    chunk_size = 45000
-                    # handle split ?
-                    payload = bytes.fromhex( rtp_headers['payload'] )
-                    if len( payload ) < chunk_size :
-                        rtp_headers['sequence_number'] = cseq_96
-                        cseq_96 = cseq_96 + 1
-                        rtp_data = bytes.fromhex( rtp_work.GenerateRTPpacket2( rtp_headers, bytes.hex( payload ) ) )
-                        targetSocket.sendto( rtp_data, ( udp_address, udp_port ) )
-                    else :
-                        chunk_start = 0
-                        while chunk_start < len( payload ) :
-                            #
-                            payload_block = payload[chunk_start:chunk_size]
-                            chunk_start = chunk_start + chunk_size
-                            #
-                            rtp_headers['sequence_number'] = cseq_96
-                            cseq_96 = cseq_96 + 1
-                            rtp_data = bytes.fromhex( rtp_work.GenerateRTPpacket2( rtp_headers, bytes.hex( payload_block ) ) )
-                            targetSocket.sendto( rtp_data, ( udp_address, udp_port ) )
-                            #
-                            
-                            
-
-                #
+                # read next marker $
                 tb = self.readBytes( response.raw, 1 )
-                packet_num = packet_num + 1
 
-                #
-            # checkbyte = response.iter_content( 1, False )
-            # state = 0            
-            # for byte in checkbyte :
-            #     if state == 0 :
-            #         if  byte == '$' :
-            #             print( "symbol is ok" )
-            #         state = 1
-            #     elif state == 1 :
-            #         #print( "not ok" )
-            #         pass
+            else :
 
-            #response.raw
+                print( "non matching $ found" )
 
-            print( "non matching $ found or thread break" )
-
-            #response.iter_content( )
-
-
-#        response.close()
-
-        #pass
 
     def do_PLAY( self ) :
         # проверить пераметры session
@@ -283,28 +265,22 @@ class S(UpdatedRequestHandler):
         # добавить эту сессию к списку слушателей для потока
         # запустить поток, если не запущен
         # сказать клиенту ок 
-        #print( "play" )
-        #print( self.client_address )
-        #print( self.headers )
-
-        cseq = self.headers["CSeq"]
         session = self.headers["Session"]
-        #transport = self.headers["Transport"]
-
         if session in self.sessionList :
             self.send_response( 200 )
             #
             sessionInfo = self.sessionList[session]
-            event = threading.Event()
-            thread = threading.Thread( target=self.playThread, args=( sessionInfo["udp_address"], sessionInfo["udp_port"], event ), daemon=True )
-            thread.start()
-            # store threar for cleaning up?
-            sessionInfo["thread"] = thread
-            sessionInfo["thread_event"] = event
+            # if not already playing
+            if (not "thread" in sessionInfo) or (sessionInfo["thread"] is None) :
+                # store threar for cleaning up?
+                sessionInfo["thread"] = thread = threading.Thread( target=self.playThread, args=( sessionInfo, ), daemon=True )
+                sessionInfo["thread_event"] = threading.Event()
+                #
+                thread.start()
         else :
             self.send_response( 404 )
 
-        self.send_header( "CSeq", cseq )
+        self.send_header( "CSeq", self.headers["CSeq"] )
         self.end_headers()
 
     #
@@ -314,22 +290,17 @@ class S(UpdatedRequestHandler):
         # если есть, то убрать сессию из списка слушателей
         # если это последний слушатель, то остановить поток
         # ответить клиенту
-        #print( "teardown" )
-        #print( self.client_address )
-        #print( self.headers )
-        cseq = self.headers["CSeq"]
         session = self.headers["Session"]
-        #
         if session in self.sessionList :
             sessionInfo = self.sessionList.pop( session )
             sessionInfo["thread_event"].set()
+            #
+            sessionInfo["thread"] = None
+            sessionInfo["thread_event"] = None
         #
-        print( f"teardown: session {session}" )
-        #transport = self.headers["Transport"]
+        #print( f"teardown: session {session}" )
         self.send_response( 200 )
-        self.send_header( "CSeq", cseq )
-        #self.send_header( "Session", session )
-        #self.send_header( "Transport", transport + ";server_port=15678-15679")
+        self.send_header( "CSeq", self.headers["CSeq"] )
         self.end_headers()
         pass
 
@@ -338,7 +309,16 @@ class S(UpdatedRequestHandler):
     #     self.wfile.write(self._html("hi!"))
 
     def do_OPTIONS( self ) :
-        self.notimplemented()
+        #self.notimplemented()
+        #print( f"options, {self.headers}" )
+        #transport = self.headers["Transport"]
+        self.send_response( 200 )
+        self.send_header( "CSeq", self.headers["CSeq"] )
+        self.send_header( "Public", "DESCRIBE, SETUP, TEARDOWN, PLAY, OPTIONS" )
+        #self.send_header( "Session", session )
+        #self.send_header( "Transport", transport + ";server_port=15678-15679")
+        self.end_headers()
+
         #return
         # sliently ignore
         #print( "options" )
@@ -377,7 +357,7 @@ class S(UpdatedRequestHandler):
         self.notimplemented()
 
 
-def run(server_class=http.server.HTTPServer, handler_class=S, addr="localhost", port=8000):
+def run(server_class=http.server.HTTPServer, handler_class=S, addr="localhost", port=554):
     server_address = (addr, port)
     print(f"Starting httpd server on {addr}:{port}")
     httpd = server_class(server_address, handler_class)
@@ -393,14 +373,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "-l",
         "--listen",
-        default="localhost",
+        default="0.0.0.0",
         help="Specify the IP address on which the server listens",
     )
     parser.add_argument(
         "-p",
         "--port",
         type=int,
-        default=8000,
+        default=554,
         help="Specify the port on which the server listens",
     )
     args = parser.parse_args()
